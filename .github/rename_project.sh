8d3eb8bb2dc928c476bafcbfe0c308fbfb650f8b
clockworkpc <clockworkpc@users.noreply.github.com>
2023-08-28 07:09:08 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="clockworkpc"
+original_name="python_puzzles"
+original_urlname="python_puzzles"
+original_description="Awesome python_puzzles created by clockworkpc"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv python_puzzles $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ python_puzzles/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for python_puzzles."""
+
+from python_puzzles.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
